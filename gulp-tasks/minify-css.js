580121dae9383e584aa3f859b52b9a4ea337fb69
Alan Shortis <alan.shortis@mac.com>
2018-03-16 22:40:38 +0000

    Create minify task

---
+++ gulp-tasks/minify-css.js
@@ -0,0 +1,20 @@
+// Require package information.
+const pkg = require('../package.json');
+const { dest } = pkg.paths;
+
+
+// Require NPM packages.
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const cssnano = require('gulp-cssnano');
+
+
+// Minify production CSS.
+gulp.task('minify', ['css'], () => {
+  return gulp.src(`${dest.css}/**/*.css`)
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(cssnano())
+    .pipe(gulp.dest(dest.css));
+});
