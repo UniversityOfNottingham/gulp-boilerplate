bb39743d47aea31fd900203039f4b719de9edc54
Alan Shortis <alan.shortis@mac.com>
2018-03-16 22:31:11 +0000

    Create sasslint task

---
+++ gulp-tasks/sasslint.js
@@ -0,0 +1,24 @@
+// Require package information.
+const pkg = require('../package.json');
+const { src } = pkg.paths;
+
+
+// Require NPM packages.
+const gulp = require('gulp');
+const sassLint = require('gulp-sass-lint');
+const notifier = require('node-notifier');
+
+
+// Check the content of each .scss file against the rules defined in .sass-lint.yml.
+gulp.task('sasslint', () => {
+  return gulp.src(`${src.sass}/**/*.scss`)
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .on('error', () => {
+      notifier.notify({
+        title: 'Gulp',
+        message: 'SASS linting failed'
+      });
+    });
+});
