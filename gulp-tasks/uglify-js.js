fa0735ca580a664bc560c7106c752bb035bede75
Alan Shortis <alan.shortis@mac.com>
2018-03-16 16:02:15 +0000

    Create uglify task

---
+++ gulp-tasks/uglify-js.js
@@ -0,0 +1,20 @@
+// Require package information.
+const pkg = require('../package.json');
+const { dest } = pkg.paths;
+
+
+// Require NPM packages.
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+
+
+// Uglify production JS.
+gulp.task('uglify', ['js'], () => {
+  return gulp.src([`${dest.js}/*.js`, `!${dest.js}/*.min.js`])
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(dest.js));
+});
