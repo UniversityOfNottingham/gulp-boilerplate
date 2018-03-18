083c58fb3e6a5ae693ffe66b87211c2c43d63d0c
Alan Shortis <alan.shortis@mac.com>
2018-03-18 09:16:06 +0000

    Add mangle option

--- gulp-tasks/uglify-js.js
+++ gulp-tasks/uglify-js.js
@@ -1,21 +1,24 @@
 // Require package information.
-const pkg = require("../package.json");
+const pkg = require('../package.json');
 const { dest } = pkg.paths;
 
 // Require NPM packages.
-const gulp = require("gulp");
-const rename = require("gulp-rename");
-const uglify = require("gulp-uglify");
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
 // Uglify production JS.
-gulp.task("uglify", ["js"], () => {
-  return gulp
-    .src([`${dest.js}/*.js`, `!${dest.js}/*.min.js`])
-    .pipe(
-      rename({
-        suffix: ".min"
-      })
-    )
-    .pipe(uglify())
-    .pipe(gulp.dest(dest.js));
+gulp.task('uglify', ['js'], () => {
+  return (
+    gulp
+      .src([`${dest.js}/*.js`, `!${dest.js}/*.min.js`])
+      .pipe(
+        rename({
+          suffix: '.min'
+        })
+      )
+      // Set mangle to false to preserve variable names.
+      .pipe(uglify({ mangle: true }))
+      .pipe(gulp.dest(dest.js))
+  );
 });
